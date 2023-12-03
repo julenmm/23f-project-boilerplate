a71d92d085432864a4a5c3496351dbcb1ae5bcb6
julenmm <julenmm27@gmail.com>
2023-12-03 17:07:37 -0500

    fesasd

--- flask-app/src/front_desk_agent/front_desk_agent.py
+++ flask-app/src/front_desk_agent/front_desk_agent.py
@@ -17,35 +17,44 @@ def get_customers():
         json_data.append(dict(zip(row_headers, row)))
     return jsonify(json_data)
 
-# Update customer preferences with a particular userID
-@front_desk_agent.route('/Preference/<customerId>', methods=['PUT'])
-def put_customer_pref(customerId):
+# Add customer preferences for a particular userID
+@front_desk_agent.route('/Preference/<customerId>', methods=['POST'])
+def post_customer_pref(customerId):
     try:
         pref = request.json  # Get the new preferences from the request body
         cursor = db.get_db().cursor()
 
-        # Update the customer's preferences in the database
-        update_query = "UPDATE Preference SET preferences = %s WHERE customerId = %s"
-        cursor.execute(update_query, (json.dumps(pref), customerId))
+        # Check if preferences already exist for the customer
+        check_query = "SELECT * FROM Preference WHERE customerId = %s;"
+        cursor.execute(check_query, (customerId,))
+        existing_pref = cursor.fetchone()
 
-        db.get_db().commit()  # Commit the transaction
+        if existing_pref:
+            # Preferences already exist, you can choose to update or return a message
+            return jsonify({"message": "Preferences already exist for this customer."}), 409
+        else:
+            # Insert the new preferences into the database
+            insert_query = "INSERT INTO Preference (preferences, customerId) VALUES (%s, %s);"
+            cursor.execute(insert_query, (json.dumps(pref), customerId))
+            db.get_db().commit()  # Commit the transaction
 
-        # Fetch the updated customer data to return in the response
-        cursor.execute("SELECT * FROM Preference WHERE customerId = %s", (customerId,))
-        row_headers = [x[0] for x in cursor.description]
-        theData = cursor.fetchall()
-        json_data = [dict(zip(row_headers, row)) for row in theData]
+            # Fetch the newly added preferences to return in the response
+            cursor.execute("SELECT * FROM Preference WHERE customerId = %s;", (customerId,))
+            row_headers = [x[0] for x in cursor.description]
+            theData = cursor.fetchall()
+            json_data = [dict(zip(row_headers, row)) for row in theData]
 
-        response = make_response(jsonify(json_data))
-        response.status_code = 200
-        response.mimetype = 'application/json'
-        return response
+            response = make_response(jsonify(json_data))
+            response.status_code = 201
+            response.mimetype = 'application/json'
+            return response
+
     except Exception as e:
         db.get_db().rollback()  # Rollback in case of any error
         return jsonify({"error": str(e)}), 500
-    
 
-@front_desk_agent.route('/bookings/new', methods=['POST'])
+    
+@front_desk_agent.route('/bookings', methods=['POST'])
 def add_booking():
     try:
         # Extract booking information from request body
@@ -55,10 +64,14 @@ def add_booking():
         hotel_id = data['hotel_id']
         occupancy_start_date = data['occupancy_start_date']
         occupancy_end_date = data['occupancy_end_date']
-        
+
+        # Validate date order
+        if occupancy_start_date >= occupancy_end_date:
+            return jsonify({"error": "Occupancy start date must be before end date"}), 400
+
         # Check if customer exists
         cursor = db.get_db().cursor()
-        cursor.execute("SELECT * FROM Customer WHERE CustomerId = %s", (customer_id,))
+        cursor.execute("SELECT * FROM Customer WHERE CustomerId = %s;", (customer_id,))
         customer = cursor.fetchone()
         if not customer:
             return jsonify({"error": "Customer not found"}), 404
@@ -67,10 +80,10 @@ def add_booking():
         cursor.execute("""
             SELECT * FROM Booking 
             WHERE RoomNum = %s AND HotelId = %s AND (
-                (%s BETWEEN OccupancyStartDate AND OccupancyEndDate) OR 
-                (%s BETWEEN OccupancyStartDate AND OccupancyEndDate)
+                (OccupancyStartDate < %s AND OccupancyEndDate > %s) OR
+                (OccupancyStartDate < %s AND OccupancyEndDate > %s)
             )
-        """, (room_num, hotel_id, occupancy_start_date, occupancy_end_date))
+        """, (room_num, hotel_id, occupancy_end_date, occupancy_start_date, occupancy_start_date, occupancy_end_date))
         booking = cursor.fetchone()
         if booking:
             return jsonify({"error": "Room is not available for the selected dates"}), 400
@@ -78,15 +91,15 @@ def add_booking():
         # Insert new booking
         cursor.execute("""
             INSERT INTO Booking (RoomNum, HotelId, CustomerId, OccupancyStartDate, OccupancyEndDate) 
-            VALUES (%s, %s, %s, %s, %s)
+            VALUES (%s, %s, %s, %s, %s);
         """, (room_num, hotel_id, customer_id, occupancy_start_date, occupancy_end_date))
         db.get_db().commit()
 
-        return jsonify({"message": "Booking successfully added"}), 201
+        return jsonify({"message": "Booking successfully added", "booking_details": data}), 201
+
     except Exception as e:
         db.get_db().rollback()  # Rollback in case of any error
         return jsonify({"error": str(e)}), 500
-
 
 # Get all the prefferences for all customers
 @front_desk_agent.route('/preferences', methods=['GET'])
@@ -112,20 +125,20 @@ def get_preferences():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-# Delete a customers preferences    
+# Delete a customer's preferences    
 @front_desk_agent.route('/preferences/<customerID>', methods=['DELETE'])
 def delete_customer_preferences(customerID):
     try:
         cursor = db.get_db().cursor()
         
         # First, check if the customer exists
-        cursor.execute("SELECT * FROM Customer WHERE CustomerId = %s", (customerID,))
+        cursor.execute("SELECT * FROM Customer WHERE CustomerId = %s;", (customerID,))
         customer = cursor.fetchone()
         if not customer:
             return jsonify({"error": "Customer not found"}), 404
         
         # If the customer exists, delete their preferences
-        delete_query = "DELETE FROM Preference WHERE CustomerId = %s"
+        delete_query = "DELETE FROM Preference WHERE CustomerId = %s;"
         cursor.execute(delete_query, (customerID,))
         db.get_db().commit()
 
