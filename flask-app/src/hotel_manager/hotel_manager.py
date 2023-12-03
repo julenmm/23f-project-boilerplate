4874c87f7fc4762862268c984e2710a0ade7eca9
Anya Trubelja <anyatrubelja@Anyas-MacBook-Air.local>
2023-12-03 13:12:28 -0500

    new route for hotel mannager

--- flask-app/src/hotel_manager/hotel_manager.py
+++ flask-app/src/hotel_manager/hotel_manager.py
@@ -35,3 +35,41 @@ def get_customer(userID):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+rooms = Blueprint("rooms", __name__)
+
+@rooms.route('/rooms/occupancy', methods=['GET'])
+def get_rooms_occupancy():
+    try:
+        cursor = db.get_db().cursor()
+        current_date = datetime.datetime.now().date()  # Get the current date
+
+        # Query to select room information and occupancy status
+        query = """
+        SELECT Room.roomNum, Room.hotelId, Room.cleaned, Room.occupancy, Room.yearlyMaintenance, Room.roomPrice,
+               CASE
+                   WHEN Booking.roomNum IS NOT NULL AND Booking.hotelId = Room.hotelId AND 
+                        Booking.occupancyStartDate <= %s AND Booking.occupancyEndDate >= %s 
+                   THEN TRUE
+                   ELSE FALSE
+               END as IsOccupied
+        FROM Room
+        LEFT JOIN Booking ON Room.roomNum = Booking.roomNum AND Room.hotelId = Booking.hotelId
+        AND (Booking.occupancyStartDate <= %s AND Booking.occupancyEndDate >= %s)
+        GROUP BY Room.roomNum, Room.hotelId
+        ORDER BY Room.hotelId, Room.roomNum
+        """
+        cursor.execute(query, (current_date, current_date, current_date, current_date))
+        row_headers = [x[0] for x in cursor.description]  # Extract column headers
+        results = cursor.fetchall()
+        
+        # Convert query results to json format
+        json_data = []
+        for result in results:
+            room_data = dict(zip(row_headers, result))
+            room_data['IsOccupied'] = bool(room_data['IsOccupied'])  # Convert to boolean
+            json_data.append(room_data)
+
+        return jsonify(json_data), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
