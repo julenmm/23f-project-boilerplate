f244ceb43d73e83e2eecc1af763875c1bb2e876f
Whaleddddd <helaric.425@gmail.com>
2023-12-03 17:03:28 -0500

    hotel manager customer

--- flask-app/src/hotel_manager/hotel_manager.py
+++ flask-app/src/hotel_manager/hotel_manager.py
@@ -9,8 +9,7 @@ hotel_manager = Blueprint('hotel manager', __name__)
 @hotel_manager.route('/Customers', methods=['GET'])
 def get_customers():
     cursor = db.get_db().cursor()
-    cursor.execute('select company, last_name,\
-        first_name, job_title, business_phone from Customers;')
+    cursor.execute('select lastName, firstName from Customer;')
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -19,7 +18,7 @@ def get_customers():
     return json_data
 
 # Get customer detail for customer with particular userID
-@hotel_manager.route('/customers/<userID>', methods=['GET'])
+@hotel_manager.route('/Customers/<customerId>', methods=['GET'])
 def get_customer(userID):
     cursor = db.get_db().cursor()
     cursor.execute('select * from customers where id = {0}'.format(userID))
@@ -28,7 +27,4 @@ def get_customer(userID):
     theData = cursor.fetchall()
     for row in theData:
         json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
+    return json_data
