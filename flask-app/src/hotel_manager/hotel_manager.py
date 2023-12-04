74593dc1172d055f13ee61e8f8f44ca4e2511a33
Jet Ulven <jetulven@yahoo.com>
2023-12-04 16:45:46 -0500

    hotel manager update

--- flask-app/src/hotel_manager/hotel_manager.py
+++ flask-app/src/hotel_manager/hotel_manager.py
@@ -131,9 +131,9 @@ def get_shift():
 
 
 
-#change employee shift 
-@hotel_manager.route('/change_shift', methods=['POST'])
-def change_employee_shift():
+#add employee shift 
+@hotel_manager.route('/add_shift', methods=['POST'])
+def add_employee_shift():
     try:
         data = request.get_json()  # Get JSON data from the request body
         if not data or 'employeeId' not in data or 'dateTimeStart' not in data or 'dateTimeEnd' not in data:
@@ -145,28 +145,19 @@ def change_employee_shift():
 
         cursor = db.get_db().cursor()
 
-        # Check if the shift exists
+        # Insert new shift
+        insert_query = "INSERT INTO Shift (employeeId, dateTimeStart, dateTimeEnd) VALUES (%s, %s, %s);"
+        cursor.execute(insert_query, (employee_id, new_start_time, new_end_time))
+        db.get_db().commit()  # Commit the transaction
+
+        # Fetch the added shift
         check_query = "SELECT * FROM Shift WHERE employeeId = %s AND dateTimeStart = %s;"
-        cursor.execute(check_query, (employee_id, new_start_time))
-        existing_shift = cursor.fetchone()
-
-        if existing_shift:
-            # Shift exists, update it with new end time
-            update_query = "UPDATE Shift SET dateTimeEnd = %s WHERE employeeId = %s AND dateTimeStart = %s;"
-            cursor.execute(update_query, (new_end_time, employee_id, new_start_time))
-            db.get_db().commit()  # Commit the transaction
-            message = "Shift updated successfully."
-        else:
-            # No shift exists for the given start time, return an error message
-            return jsonify({"error": "Shift not found"}), 404
-
-        # Fetch the updated shift
         cursor.execute(check_query, (employee_id, new_start_time))
         row_headers = [x[0] for x in cursor.description]  # this will extract row headers
         theData = cursor.fetchall()
         json_data = [dict(zip(row_headers, row)) for row in theData]
 
-        response = make_response(jsonify({"message": message, "data": json_data}), 200)
+        response = make_response(jsonify({"message": "Shift added successfully.", "data": json_data}), 201)
         response.mimetype = 'application/json'
         return response
 
