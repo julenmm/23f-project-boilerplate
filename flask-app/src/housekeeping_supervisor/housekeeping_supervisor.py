6e7cfb0c3bf45a16515aa138d28d092b37c58b1b
julenmm <julenmm27@gmail.com>
2023-12-03 20:20:12 -0500

    hosekeepingsupervisor

--- flask-app/src/housekeeping_supervisor/housekeeping_supervisor.py
+++ flask-app/src/housekeeping_supervisor/housekeeping_supervisor.py
@@ -5,12 +5,15 @@ from src import db
 
 housekeeping_supervisor = Blueprint('housekeeping supervisor', __name__)
 
-# Get all customers from the DB
-@housekeeping_supervisor.route('/customers', methods=['GET'])
-def get_customers():
+# get all rooms with their cleaned status for a hotel id
+@housekeeping_supervisor.route('/roomsCleaned', methods=['GET'])
+def get_rooms_cleaned():
+    # Get hotelId from the request body
+    data = request.get_json()
+    hotel_id = data['hotelId']
+
     cursor = db.get_db().cursor()
-    cursor.execute('select company, last_name,\
-        first_name, job_title, business_phone from customers')
+    cursor.execute('SELECT room_number, cleaned_status FROM rooms WHERE hotelId = %s', (hotel_id,))
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -21,11 +24,15 @@ def get_customers():
     the_response.mimetype = 'application/json'
     return the_response
 
-# Get customer detail for customer with particular userID
-@housekeeping_supervisor.route('/customers/<userID>', methods=['GET'])
-def get_customer(userID):
+# Return a list of all supply units in stock for a hotel
+@housekeeping_supervisor.route('/supplies', methods=['GET'])
+def get_supplies():
+    # Get hotelId from the request body
+    data = request.get_json()
+    hotel_id = data['hotelId']
+
     cursor = db.get_db().cursor()
-    cursor.execute('select * from customers where id = {0}'.format(userID))
+    cursor.execute('SELECT name, unitsInStock FROM Supplies WHERE hotelId = %s', (hotel_id,))
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -35,3 +42,42 @@ def get_customer(userID):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+
+# Return a list of all supply units in stock for a hotel
+@housekeeping_supervisor.route('/shifts', methods=['POST'])  
+def insert_employee_shift_times():
+    # Get data from the request body
+    data = request.get_json()
+    employee_id = data['employeeId']
+    start = data['start']
+    end = data['end']
+
+    cursor = db.get_db().cursor()
+    # Use placeholder %s 
+    cursor.execute('INSERT INTO Shift (employeeId, dateTimeStart, dateTimeEnd) VALUES (%s, %s, %s)', 
+                   (employee_id, start, end))
+    db.get_db().commit()  # Commit to the database
+    
+    #return success message
+    return jsonify({"message": "Shift times updated successfully"}), 201
+
+@housekeeping_supervisor.route('/updateTimeOff', methods=['PATCH'])
+def update_time_off():
+    # Get data from the request body
+    data = request.get_json()
+    employee_id = data['employeeId']
+    date_to_update = data['date']
+    new_time_off_status = data['timeOff']
+
+    cursor = db.get_db().cursor()
+    # Update the timeOff status for a specific date and employeeId
+    cursor.execute('UPDATE Shift SET timeOff = %s WHERE employeeId = %s AND DATE(dateTimeStart) = %s', 
+                   (new_time_off_status, employee_id, date_to_update))
+    db.get_db().commit()  # Commit the transaction to the database
+
+    # Return a success message
+    return jsonify({"message": "Time off status updated successfully"}), 200
+
+
+
