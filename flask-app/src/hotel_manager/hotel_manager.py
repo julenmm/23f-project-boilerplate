4fd28b455b8d315acfc91dc92f82ecf752f5c2fc
Whaleddddd <helaric.425@gmail.com>
2023-12-03 20:22:10 -0500

    hotel manager

--- flask-app/src/hotel_manager/hotel_manager.py
+++ flask-app/src/hotel_manager/hotel_manager.py
@@ -17,9 +17,9 @@ def get_customers():
         json_data.append(dict(zip(row_headers, row)))
     return json_data
 
-# Get all em from the DB
+# Get all employee from the DB
 @hotel_manager.route('/Employee', methods=['GET'])
-def get_employees():
+def get_employee():
     cursor = db.get_db().cursor()
     cursor.execute('select lastName, firstName, employeeId from Employee;')
     row_headers = [x[0] for x in cursor.description]
@@ -30,11 +30,13 @@ def get_employees():
     return json_data
 
 
+
+
 # Get all em and phonenumber
 @hotel_manager.route('/Employee_number', methods=['GET'])
-def get_employees():
+def get_employees_phone_number():
     cursor = db.get_db().cursor()
-    cursor.execute('select lastName, firstName, employeeId phoneNumber from Employee;')
+    cursor.execute('select lastName, firstName, employeeId, phoneNumber from Employee;')
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -54,3 +56,29 @@ def delete_employee():
     db.get_db().commit()
     # Return a success message
     return "Deleted Successfully"
+
+
+@hotel_manager.route('/add_employee', methods=['POST'])
+def add_employee(employeeId):
+    # get the new emloyee details from the request
+    employeeId = request.json.get("add_employeeId", None) 
+    firstName = request.json.get("add_firstname", None)
+    lastName = request.json.get("add_lastname", None)
+    phoneNumber = request.json.get("add_phoneNumber", None)
+    weeklyHours = request.json.get("add_weeklyHours", None) 
+    hourlypay = request.json.get("add_hourlyPay", None) 
+
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # construct the insert statement
+    insert_stmt = 'INSERT INTO Employee (employeeId, firstName, lastName, phoneNumber, weeklyHours, hourlypay) VALUES (' + str(employeeId) + ', "' + firstName + '", "'+ lastName + '", "' + str(phoneNumber) + ', ' + str(weeklyHours)+ ', ' + str(hourlypay) + ')'
+
+    # execute the query
+    cursor.execute(insert_stmt)
+
+    # commit the changes to the database
+    db.get_db().commit()
+
+    # return a success message
+    return "employee added successfully"
